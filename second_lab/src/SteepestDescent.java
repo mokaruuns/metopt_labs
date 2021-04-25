f86b7d5d2581c2a8fca26b9991267ba6947891ab
Yacenko_Danil <284539@niuitmo.ru>
2021-04-25 15:37:15 +0300

    SteepestDescent is works!!!

--- second_lab/src/SteepestDescent.java
+++ second_lab/src/SteepestDescent.java
@@ -26,7 +26,7 @@ public class SteepestDescent extends BiMinimalizer {
         List<Double> temp2 = new ArrayList<>();
         for(int i = 0; i < dimensions; i++){
             temp1.add(x1.get(i) - eps);
-            temp2.add(x2.get(0) + eps);
+            temp2.add(x2.get(i) + eps);
         }
         double f1 = apply(temp1);
         double f2 = apply(temp2);
@@ -40,35 +40,28 @@ public class SteepestDescent extends BiMinimalizer {
                 b = x2;
                 x2 = x1;
                 f2 = f1;
-                double temp_x1 = a.get(0) + (b.get(0) - a.get(0)) * t1;
-                x1.remove(0);
-                x1.add(temp_x1);
-                temp_x1 = a.get(1) + (b.get(1) - a.get(1)) * t1;
-                x1.remove(1);
-                x1.add(temp_x1);
-                List<Double> temp = List.copyOf(x1);
-                f1 = apply(temp);
+                for (int i = 0; i < a.size(); i++){
+                    x1.set(i, a.get(i) + (b.get(i) - a.get(i)) * t1);
+                }
+                f1 = apply(x1);
             } else {
                 a = x1;
                 x1 = x2;
                 f1 = f2;
-                double temp_x2 = a.get(0) + (b.get(0) - a.get(0)) * t2;
-                x2.remove(0);
-                x2.add(temp_x2);
-                temp_x2 = a.get(1) + (b.get(1) - a.get(1)) * t2;
-                x2.remove(1);
-                x2.add(temp_x2);
-                List<Double> temp = List.copyOf(x2);
-                f2 = apply(temp);
+                for (int i = 0; i < a.size(); i++){
+                    x2.set(i, a.get(i) + (b.get(i) - a.get(i)) * t2);
+                }
+                f2 = apply(x2);
             }
+            res = 0;
             for(int i = 0; i < dimensions; i++)
             {
                 res += (b.get(i) - a.get(i)) * (b.get(i) - a.get(i));
             }
         }
         List<Double> ans = new ArrayList<>();
-        ans.add((a.get(0) + b.get(0)) / 2);
-        ans.add((a.get(1) + b.get(1)) / 2);
+        for(int i = 0; i < dimensions; i++)
+            ans.add((a.get(i) + b.get(i)) / 2);
         return ans;
     }
 
@@ -77,10 +70,9 @@ public class SteepestDescent extends BiMinimalizer {
         boolean stop = false;
         int iter = 0;
         ArrayList<Double> args1 = new ArrayList<>(Collections.nCopies(dimensions, 1.0));
-        ArrayList<Double> startPoint = new ArrayList<>(Collections.nCopies(dimensions, 1.0));
 
         while (!stop) {
-            List<Double> grad = countGradient(startPoint);
+            List<Double> grad = countGradient(args1);
             List<Double> minusGrad = new ArrayList<>(List.copyOf(grad));
             for (int i = 0; i < minusGrad.size(); i++) {
                 minusGrad.set(i, minusGrad.get(i) * (-1));
@@ -96,11 +88,11 @@ public class SteepestDescent extends BiMinimalizer {
                 stop = true;
             }
 
-            startPoint = new ArrayList<>(args1);
+            args1 = new ArrayList<>(args2);
             iter += 1;
         }
 
-        return startPoint;
+        return args1;
     }
 
 }
