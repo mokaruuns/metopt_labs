7ace31f99702a4c6cd2a9373fcc9eb54c7cb3cdc
brenscrazy <glebmanufa@gmail.com>
2021-04-25 16:44:39 +0300

    fletcher reeves is done

---
+++ second_lab/src/FletcherReeves.java
@@ -0,0 +1,33 @@
+import java.util.Collections;
+import java.util.List;
+
+public class FletcherReeves extends BiMinimalizer {
+
+    FletcherReeves(List<List<Double>> a, List<Double> b, double c, int dimensions) {
+        super(a, b, c, dimensions);
+    }
+
+    @Override
+    List<Double> minimalize() {
+        return fletcherReevesMethod();
+    }
+
+
+    private List<Double> fletcherReevesMethod() {
+        List<Double> xk = Collections.nCopies(dimensions, 0.0);
+        List<Double> gradK = countGradient(xk);
+        List<Double> pk = mulOnNumber(gradK, -1);
+        double alfak = 0;
+        double betak = 0;
+        for (int i = 0; i < dimensions; i++) {
+            List<Double> apk = matrixVectorMul(a, pk);
+            alfak = mul(gradK, gradK) / mul(apk, pk);
+            xk = sum(xk, mulOnNumber(pk, alfak));
+            List<Double> gradK1 = sum(gradK, mulOnNumber(apk, alfak));
+            betak = mul(gradK1, gradK1) / mul(gradK, gradK);
+            pk = sum(mulOnNumber(gradK1, -1), mulOnNumber(pk, betak));
+            gradK = gradK1;
+        }
+        return xk;
+    }
+}
