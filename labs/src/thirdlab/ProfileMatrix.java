94403354b3793283fd3b475363737b78db520c12
brenscrazy <glebmanufa@gmail.com>
2021-05-19 20:40:33 +0300

    optimise

--- labs/src/thirdlab/ProfileMatrix.java
+++ labs/src/thirdlab/ProfileMatrix.java
@@ -13,18 +13,29 @@ public class ProfileMatrix implements Matrix {
     private final List<List<Double>> au;
     private final List<List<Double>> al;
     private final List<Double> di;
+    private final List<Integer> startsForL;
+    private final List<Integer> startsForU;
 
 
     public ProfileMatrix(List<List<Double>> m, int n) {
         au = new ArrayList<>();
         al = new ArrayList<>();
         di = new ArrayList<>();
+        startsForL = new ArrayList<>();
+        startsForU = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            startsForU.add(i);
+            startsForL.add(i);
+        }
         for (int i = 0; i < n; i++) {
             al.add(new ArrayList<>());
             boolean flag = false;
             for (int j = 0; j < i; j++) {
                 double elem = m.get(i).get(j);
                 if (flag || m.get(i).get(j) != 0) {
+                    if (!flag) {
+                        startsForL.set(i, j);
+                    }
                     flag = true;
                     al.get(i).add(elem);
                 }
@@ -36,6 +47,9 @@ public class ProfileMatrix implements Matrix {
             for (int i = 0; i < j; i++) {
                 double elem = m.get(i).get(j);
                 if (flag || m.get(i).get(j) != 0) {
+                    if (!flag) {
+                        startsForU.set(j, i);
+                    }
                     flag = true;
                     au.get(j).add(elem);
                 }
@@ -137,22 +151,22 @@ public class ProfileMatrix implements Matrix {
         isLU = true;
         int n = al.size();
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
+            for (int j = startsForL.get(i); j < i; j++) {
                 double a = get(i, j);
-                for (int k = 0; k < j; k++) {
+                for (int k = Math.max(startsForL.get(j), startsForU.get(i)); k < j; k++) {
                     a -= getFromL(i, k) * getFromU(k, j);
                 }
                 set(i, j, a);
             }
-            for (int j = 0; j < i; j++) {
+            for (int j = startsForU.get(i); j < i; j++) {
                 double a = get(j, i);
-                for (int k = 0; k < j; k++) {
+                for (int k = Math.max(startsForL.get(j), startsForU.get(i)); k < j; k++) {
                     a -= getFromL(j, k) * getFromU(k, i);
                 }
                 set(j, i, a / get(j, j));
             }
             double a = get(i, i);
-            for (int k = 0; k < i; k++) {
+            for (int k = Math.max(startsForL.get(i), startsForU.get(i)); k < i; k++) {
                 a -= getFromL(i, k) * getFromU(k, i);
             }
             set(i, i, a);
@@ -165,8 +179,8 @@ public class ProfileMatrix implements Matrix {
         }
         int n = au.size();
         List<Double> bnew = new ArrayList<>(b);
-        for (int k = 0; k < n - 1; k++) {
-            for (int i = k + 1; i < n; i++) {
+        for (int i = 1; i < n; i++) {
+            for (int k = startsForL.get(i); k < i; k++) {
                 double temp = getFromL(i, k) / getFromL(k, k);
                 bnew.set(i, bnew.get(i) - temp * bnew.get(k));
             }
@@ -179,25 +193,27 @@ public class ProfileMatrix implements Matrix {
         for (int i = 0; i < n; i++) {
             ans.add(0.0);
         }
-        ans.set(n - 1, y.get(n - 1)/getFromU(n - 1, n - 1));
-        for (int k = n - 2; k >= 0; k--) {
+        ans.set(n - 1, y.get(n - 1) / getFromU(n - 1, n - 1));
+        for (int k = n - 1; k > 0; k--) {
             double sum = 0;
-            for (int j = k + 1; j < n; j++) {
-                sum += getFromU(k, j) * ans.get(j);
+            for (int j = startsForU.get(k); j < k; j++) {
+                y.set(j, y.get(j) - ans.get(k) * getFromU(j, k));
             }
-            ans.set(k, (y.get(k) - sum) / getFromU(k, k));
+            ans.set(k - 1, y.get(k - 1));
         }
         return ans;
     }
 
     public static void main(String[] args) {
         ProfileMatrix a = new ProfileMatrix(
-                List.of(List.of(1.0, 2.0, 3.0),
+                List.of(List.of(1.0, 0.0, 3.0),
                         List.of(4.0, 7.0, 6.0),
-                        List.of(7.0, 8.0, 9.0)
+                        List.of(0.0, 8.0, 9.0)
                 ), 3);
-
+        System.out.println(a.startsForL);
+        System.out.println(a.startsForU);
         a.turnTOLU();
+
         System.out.println(a.mul());
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
