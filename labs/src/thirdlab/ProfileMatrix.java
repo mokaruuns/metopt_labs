ede69fab3aba4b160ea7980e8f242e31e91d00b4
brenscrazy <glebmanufa@gmail.com>
2021-05-19 19:05:47 +0300

    slau reshaet vrode

--- labs/src/thirdlab/ProfileMatrix.java
+++ labs/src/thirdlab/ProfileMatrix.java
@@ -159,22 +159,53 @@ public class ProfileMatrix implements Matrix {
         }
     }
 
+    public List<Double> solveSLAU(List<Double> b) {
+        if (!isLU) {
+            turnTOLU();
+        }
+        int n = au.size();
+        List<Double> bnew = new ArrayList<>(b);
+        for (int k = 0; k < n - 1; k++) {
+            for (int i = k + 1; i < n; i++) {
+                double temp = getFromL(i, k) / getFromL(k, k);
+                bnew.set(i, bnew.get(i) - temp * bnew.get(k));
+            }
+        }
+        List<Double> y = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            y.add(bnew.get(i) / di.get(i));
+        }
+        List<Double> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            ans.add(0.0);
+        }
+        ans.set(n - 1, y.get(n - 1)/getFromU(n - 1, n - 1));
+        for (int k = n - 2; k >= 0; k--) {
+            double sum = 0;
+            for (int j = k + 1; j < n; j++) {
+                sum += getFromU(k, j) * ans.get(j);
+            }
+            ans.set(k, (y.get(k) - sum) / getFromU(k, k));
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         ProfileMatrix a = new ProfileMatrix(
-                List.of(List.of(1.0, 2.0, 3.0, 3.0),
-                        List.of(6.0, 5.0, 8.0, 7.0),
-                        List.of(7.0, 1.0, 9.0, 9.0),
-                        List.of(3.0, 4.0, 1.0, 2.0)
-                ), 4);
+                List.of(List.of(1.0, 2.0, 3.0),
+                        List.of(4.0, 7.0, 6.0),
+                        List.of(7.0, 8.0, 9.0)
+                ), 3);
 
         a.turnTOLU();
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
+        System.out.println(a.mul());
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 System.out.print(a.get(i, j) + " ");
             }
             System.out.println();
         }
-        System.out.println(a.mul());
+        System.out.println(a.solveSLAU(List.of(1.0, 1.0, 1.0)));
     }
 
 }
