5c743ed88d1e5ec87d38b0ecc884301cd5b995b4
Marsel Akhmetov <marsel.ahmetov974@gmail.com>
2021-05-23 20:56:43 +0300

    added checker for 3.3 task

--- labs/src/thirdlab/Research.java
+++ labs/src/thirdlab/Research.java
@@ -1,24 +1,22 @@
 package thirdlab;
 
-import secondLab.Matrix;
 import secondLab.NumberVector;
 import secondLab.UsualMatrix;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Research {
 
-    List<List<Double>> generate(int k, int n) {
+    private UsualMatrix generate(int size, int conditionality) {
         int start = -4, end = 0;
         double sum = 0;
         List<List<Double>> matrix = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < size; i++) {
             List<Double> temp = new ArrayList<>();
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < size; j++) {
                 double random_element = (int) (start + (Math.random() * ((end - start) + 1)));
                 if (i != j) {
                     sum += random_element;
@@ -27,14 +25,26 @@ public class Research {
             }
             matrix.add(temp);
         }
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < size; i++) {
             if (i > 0) {
                 matrix.get(i).set(i, -sum);
             } else {
-                matrix.get(i).set(i, -sum + Math.pow(10, -k));
+                matrix.get(i).set(i, -sum + Math.pow(10, -conditionality));
             }
         }
-        return matrix;
+        return new UsualMatrix(matrix);
+    }
+
+    private UsualMatrix generate(int size) {
+        List<List<Double>> matrix = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            List<Double> temp = new ArrayList<>();
+            for (int j = 0; j < size; j++) {
+                temp.add(1.0 / ((i + 1) + (j + 1) - 1));
+            }
+            matrix.add(temp);
+        }
+        return new UsualMatrix(matrix);
     }
 
     public NumberVector solve(UsualMatrix matrix, NumberVector numberVector, int n) {
@@ -49,13 +59,30 @@ public class Research {
         return new NumberVector(a.solveSLAU(numberVector.getVector()));
     }
 
-    public void run(int n, int k) {
-//        System.out.println("Размер: " + n + ", число обусловленности: " + k);
-        UsualMatrix matrixA = new UsualMatrix(generate(k, n));
-//        matrixA.printMatrix();
-        NumberVector vectorX = new NumberVector(IntStream.range(1, n + 1).mapToDouble(z -> (double) z).boxed().collect(Collectors.toList()));
-//        vectorX.printNumberVector();
-        checkLoss(n, k, vectorX, solve(matrixA, vectorX, n));
+    public void run(int size, int conditionality) {
+        UsualMatrix matrixA;
+        if (conditionality != -1) {
+            matrixA = generate(size, conditionality);
+        } else {
+            matrixA = generate(size);
+        }
+        NumberVector vectorX = new NumberVector(IntStream.range(1, size + 1).mapToDouble(z -> (double) z).boxed().collect(Collectors.toList()));
+        checkLoss(size, conditionality, vectorX, solve(matrixA, vectorX, size));
+    }
+
+
+    public void runAll(int maxSize, int maxConditionality) {
+        for (int n = 10; n <= maxSize; n *= 10) {
+            for (int k = 0; k <= maxConditionality; k++) {
+                run(n, k);
+            }
+        }
+    }
+
+    public void runAll(int maxSize) {
+        for (int n = 10; n <= maxSize; n *= 10) {
+            run(n, -1);
+        }
     }
 
     private void checkLoss(int n, int k, NumberVector expectedSolution, NumberVector currentSolution) {
@@ -65,11 +92,7 @@ public class Research {
 
     public static void main(String[] args) {
         Research research = new Research();
-        for (int c = 1; c <= 3; c++) {
-            for (int k = 0; k < 10; k++) {
-                research.run((int) Math.pow(10, c), k);
-            }
-        }
+        research.runAll(1000, 10);
     }
 
 }
